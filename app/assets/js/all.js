ce18894f4d59002b8f2945e8fa240a85619d0a2e
“pony0303” <pcd1118@gmail.com>
2022-08-26 22:33:18 +0800

    更新

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,7 @@
-console.log('Hello!');
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+$(document).ready(function () {
+  $('.ham-icon').click(function (e) {
+    e.preventDefault();
+    $('.dropdown').toggleClass('active');
+  });
 });
